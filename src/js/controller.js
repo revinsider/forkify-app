dbddd0da99c7d838e081f31c337f4080b8ae6921
revinsider <dev@makeover.com.pl>
2022-05-24 12:19:35 +0200

    mvc architecture - first changes

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,5 @@
+import * as model from './model.js';
+
 import icons from 'url:../img/icons.svg';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -34,32 +36,11 @@ const showRecipe = async function () {
     console.log(id);
 
     if (!id) return;
+    renderSpinner(recipeContainer);
 
     // 1) Loading recipe
-    renderSpinner(recipeContainer);
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    console.log(res, data);
-
-    let { recipe } = data.data;
-
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-
-    console.log(recipe);
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
 
     // Rendering recipe
     const markup = `
