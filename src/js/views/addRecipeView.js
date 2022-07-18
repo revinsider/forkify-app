b33167c769d94d1ff9d94768379f340b9144c94a
revinsider <dev@makeover.com.pl>
2022-07-18 15:56:45 +0200

    add recipe functionality first commit

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,27 @@
+import View from './View';
+import icons from '../../img/icons.svg';
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+  }
+
+  _addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', function () {
+      this._overlay.classList.toggle('hidden');
+      this._window.classList.toggle('hidden');
+    });
+  }
+
+  _generateMarkup() {}
+}
+
+export default new AddRecipeView();
