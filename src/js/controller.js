951a6fac0ff71ad34e0354dbd6a14064e1844824
revinsider <dev@makeover.com.pl>
2022-07-18 12:05:11 +0200

    bookmarks fist commit after summer break

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime';
@@ -16,6 +17,7 @@ const controlRecipes = async function () {
 
     // Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1) LOADING RECIPE
     await model.loadRecipe(id);
@@ -69,10 +71,15 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  // 1) Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.delegeBookmark(model.state.recipe.id);
-  console.log(model.state.recipe);
+
+  // 2) Update recipe view
   recipeView.update(model.state.recipe);
+
+  // 3) render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
---
+++ src/js/views/bookmarksView.js
@@ -0,0 +1,17 @@
+import View from './View';
+import previewView from './previewView';
+
+class BookmarksView extends View {
+  _parentElement = document.querySelector('.bookmarks__list');
+  _errorMessage = `No bookmarks yet. Find a favourite recipe and bookmark it :)`;
+  _successMessage;
+
+  _generateMarkup() {
+    console.log(this._data);
+    return this._data
+      .map(bookmark => previewView.render(bookmark, false))
+      .join('');
+  }
+}
+
+export default new BookmarksView();
